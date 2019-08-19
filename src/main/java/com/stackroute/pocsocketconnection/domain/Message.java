bd6a2e14c2217f1c08bb93f3e32a1ca4211a565b
sivadatta.bathina <sivadatta.bathina@cgi.com>
2019-08-19 16:18:55 +0530

    Added Lombok to Message.

--- src/main/java/com/stackroute/pocsocketconnection/domain/Message.java
+++ src/main/java/com/stackroute/pocsocketconnection/domain/Message.java
@@ -1,29 +1,14 @@
 package com.stackroute.pocsocketconnection.domain;
 
+import lombok.*;
+
+@Data
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Message {
     private String userName;
     private String message;
-    public Message() {
-    }
-
-    public Message(String userName, String message) {
-        this.userName = userName;
-        this.message = message;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
-
-    public String getUserName() {
-        return userName;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
+    private String toUser;
 }
